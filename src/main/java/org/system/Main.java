55a5cfbd449cb776d05a1e8faed8100a6f0dc680
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-10-03 17:26:49 -0300

    adiciona projeto

---
+++ src/main/java/org/system/Main.java
@@ -0,0 +1,12 @@
+package org.system;
+
+import org.system.model.Anime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+        Anime var1 = new Anime();
+        var1.setName("Naruto");
+        var1.setCreator("Masashi Kishimoto");
+    }
+}
---
+++ src/main/java/org/system/model/Anime.java
@@ -0,0 +1,22 @@
+package org.system.model;
+
+public class Anime {
+    private String name;
+    private String creator;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+}
