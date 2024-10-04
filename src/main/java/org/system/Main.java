29b97786f38d66595fa123dd2bb6f2bcc80e05c5
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2024-10-04 17:19:46 -0300

    adiciona projeto

--- src/main/java/org/system/Main.java
+++ src/main/java/org/system/Main.java
@@ -8,5 +8,9 @@ public class Main {
         Anime var1 = new Anime();
         var1.setName("Naruto");
         var1.setCreator("Masashi Kishimoto");
+        Anime var2 = new Anime();
+        var2.setName("Jujutsu Kaisen");
+        var2.setCreator("Gege Akutami");
+        System.out.println(var1.getName());
     }
 }
